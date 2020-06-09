7b7cde12d276f816202b235d5983e2c86c51f6be
吕佩刚 <lvpg@weipaitang.com>
2020-06-09 11:07:31 +0800

    ignore

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Schema;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -13,7 +14,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        // 处理 Specified key was too long ... 767 bytes 问题
+        Schema::defaultStringLength(191);
     }
 
     /**
